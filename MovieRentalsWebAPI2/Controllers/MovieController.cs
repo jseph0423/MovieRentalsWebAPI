1b5dd613a01d048a4ed68c2469ae8a05189a816b
Joseph Calatrava <calatravajoseph@gmail.com>
2022-10-23 11:53:42 +0800

    Add project files.

---
+++ MovieRentalsWebAPI2/Controllers/MovieController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieRentalsWebAPI2.Model;
+
+namespace MovieRentalsWebAPI2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        public static List<Movies> MovieList = new List<Movies>
+        {
+            new Movies
+            {
+                id = 1,
+                title="ironman",
+                description="metal",
+                isRented=false,
+                dateRented = DateTime.Now,
+                isDeleted=false,
+            },
+            new Movies
+            {
+                id = 2,
+                title="hulk",
+                description="green",
+                isRented=false,
+                dateRented = DateTime.Now,
+                isDeleted=false,
+            }
+        };
+
+
+        [HttpGet]
+        [Route("GetAll")]
+        public async Task<ActionResult<List<Movies>>> Get()
+        {
+            return Ok(MovieList);
+        }
+
+
+        [HttpGet("{id}")]
+        [Route("GetMovie")]
+        public async Task<ActionResult<Movies>> Get(int id)
+        {
+            var movie = MovieList.Find(m => m.id == id);
+            if (movie == null)
+                return BadRequest("Movie Not Found");
+            return Ok(movie);
+        }
+
+
+        [HttpPost]
+        [Route("InsertMovie")]
+        public async Task<ActionResult<List<Movies>>> NewMovie(Movies movie)
+        {
+            MovieList.Add(movie);
+            return Ok(MovieList);
+        }
+
+
+        [HttpPut]
+        public async Task<ActionResult<List<Movies>>> UpdateMovie(Movies update)
+        {
+            var movie = MovieList.Find(m => m.id == update.id);
+            if (movie == null)
+                return BadRequest("Movie Not Found");
+
+            movie.id = update.id;
+            movie.title = update.title;
+            movie.isRented = update.isRented;
+            movie.description = update.description;
+            movie.isDeleted = update.isDeleted;
+
+            return Ok(MovieList);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Movies>>> DeleteMovie(int id)
+        {
+            var movie = MovieList.Find(m => m.id==id);
+            if (movie == null)
+                return BadRequest("Movie Not Found");
+            MovieList.Remove(movie);
+            return Ok(MovieList);
+        }
+
+    }
+}
---
+++ MovieRentalsWebAPI2/Model/Movies.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieRentalsWebAPI2.Model
+{
+    public class Movies
+    {
+        public int id { get; set; }
+        public string title { get; set; } = string.Empty;
+        public string description { get; set; } = string.Empty;
+        public bool isRented { get; set; } = false;
+        public DateTime dateRented { get; set; }
+        public bool isDeleted { get; set; } = false;
+    }
+}
